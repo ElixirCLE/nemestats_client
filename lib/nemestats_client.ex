716941124ba9da5c390977d1583b285a385f62f3
Chris Rees <serneum@gmail.com>
2016-09-26 23:17:41 -0400

    Add HTTP and JSON libraries. Get communication with NemeStats working

--- lib/nemestats_client.ex
+++ lib/nemestats_client.ex
@@ -1,2 +1,16 @@
-defmodule NemestatsClient do
+defmodule NemeStatsClient do
+  require NemeStats
+
+  def login(username, password) do
+    {:ok, body} = JSX.encode(
+      %{
+        userName: username,
+        password: password,
+        preserveExistingAuthenticationToken: true
+      })
+#    NemeStats.post("UserSessions/", [body: body])
+    response = HTTPotion.post("https://nemestats.com/api/v2/UserSessions/", [body: body, headers: ["Content-Type": "application/json"]]).body
+    {:ok, session} = JSX.decode(response)
+    Enum.map(session, fn({k, v}) -> {String.to_atom(k), v} end)
+  end
 end
