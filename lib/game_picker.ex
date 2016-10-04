47802a4c372dabb76b2835e8305224aa18004842
Eric Workman <ericsparkeworkman+github@gmail.com>
2016-10-04 11:46:59 -0400

    Add credo and apply style fixes

--- lib/game_picker.ex
+++ lib/game_picker.ex
@@ -1,16 +1,29 @@
 defmodule GamePicker do
+  @moduledoc """
+  Choose a game that matches criteria
+
+  ## Examples
+      iex> games = [%Game{name: "Game1", min_players: 2, max_players: 4},
+      ...> %Game{name: "Game2", min_players: 4, max_players: 10},
+      ...> %Game{name: "Game3", min_players: 6, max_players: 12}]
+      iex> GamePicker.pick_game(games, 5)
+      %Game{name: "Game2", min_players: 4, max_players: 10}
+  """
 
   @doc """
   Picks a random game from the list.
   Returns a `%Game{}` where min_players <= num_players <= max_players.
 
   ## Examples
-      iex> games = [%Game{name: "Game1", min_players: 2, max_players: 4}, %Game{name: "Game2", min_players: 4, max_players: 10}, %Game{name: "Game3", min_players: 6, max_players: 12}]
+      iex> games = [%Game{name: "Game1", min_players: 2, max_players: 4},
+      ...> %Game{name: "Game2", min_players: 4, max_players: 10},
+      ...> %Game{name: "Game3", min_players: 6, max_players: 12}]
       iex> GamePicker.pick_game(games, 5)
       %Game{name: "Game2", min_players: 4, max_players: 10}
   """
   def pick_game(games, num_players) do
-    filter_min_players(games, num_players)
+    games
+    |> filter_min_players(num_players)
     |> filter_max_players(num_players)
     |> Enum.random
   end
@@ -20,7 +33,8 @@ defmodule GamePicker do
   Returns a filtered list of `%Game{}`s.
 
   ## Examples
-      iex> games = [%Game{name: "Game1", min_players: 2, max_players: 4}, %Game{name: "Game2", min_players: 3, max_players: 5}]
+      iex> games = [%Game{name: "Game1", min_players: 2, max_players: 4},
+      ...> %Game{name: "Game2", min_players: 3, max_players: 5}]
       iex> GamePicker.filter_min_players(games, 2)
       [%Game{name: "Game1", min_players: 2, max_players: 4}]
   """
@@ -33,7 +47,8 @@ defmodule GamePicker do
   Returns a filtered list of `%Game{}`s.
 
   ## Examples
-      iex> games = [%Game{name: "Game1", min_players: 2, max_players: 4}, %Game{name: "Game2", min_players: 3, max_players: 5}]
+      iex> games = [%Game{name: "Game1", min_players: 2, max_players: 4},
+      ...> %Game{name: "Game2", min_players: 3, max_players: 5}]
       iex> GamePicker.filter_max_players(games, 5)
       [%Game{name: "Game2", min_players: 3, max_players: 5}]
   """
