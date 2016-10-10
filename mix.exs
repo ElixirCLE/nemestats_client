3346ffe2e9aee09d0432b4fbd5e3686cbb85bf76
Chris Rees <crees@covermymeds.com>
2016-10-10 17:13:16 -0400

    Move BoardGameGeek Client to a new project

--- mix.exs
+++ mix.exs
@@ -29,7 +29,6 @@ defmodule NemeStatsClient.Mixfile do
   defp deps do
     [
       {:credo, "~> 0.4", only: [:dev, :test]},
-      {:exml, "~> 0.1"},
       {:httpotion, "~> 3.0.0"},
       {:poison, "~> 2.0"}
     ]
