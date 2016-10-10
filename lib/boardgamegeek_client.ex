de4bcc1f9cf0c9195124343cf2af1e0cdd4958bc
Chris Rees <crees@covermymeds.com>
2016-10-09 22:35:31 -0400

    Add crude retries to BGG requests

--- lib/boardgamegeek_client.ex
+++ lib/boardgamegeek_client.ex
@@ -6,6 +6,15 @@ defmodule BoardGameGeekClient do
   require BoardGameGeek
 
   def get_game_collection(username) do
-    BoardGameGeek.post("collection?username=" <> username <> "&own=1&subtype=boardgame").body
+    response = BoardGameGeek.post("collection?username=" <> username <> "&own=1&subtype=boardgame")
+    get_response(username, response, response.status_code)
+  end
+
+  defp get_response(username, _, status_code) when status_code == 202 do
+    get_game_collection(username)
+  end
+
+  defp get_response(_, response, _) do
+    response.body
   end
 end
