994e3cc97233b389f2ba5d8728ffeb26474427a0
Eric Workman <ericsparkeworkman+github@gmail.com>
2016-10-10 14:23:03 -0400

    Change player/2 to get_stats/3

--- lib/nemestats_client.ex
+++ lib/nemestats_client.ex
@@ -10,7 +10,8 @@ defmodule NemeStatsClient do
     NemeStats.post("UserSessions/", [body: body]).body
   end
 
-  def player(gaming_group, player_id) do
+  def get_stats(:player, gaming_group, player_id) do
     NemeStats.get("GamingGroups/#{gaming_group}/PlayerStats/#{player_id}/").body
   end
+  def get_stats(_, _, _), do: nil
 end
